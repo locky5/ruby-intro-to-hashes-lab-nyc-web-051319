a019f43b792a7203fe929baa73e36a91564710a9
Kevin Dai <daikevin19@yahoo.com>
2019-05-04 02:15:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,12 +9,13 @@ end
 def monopoly
   	monopoly = {
     :railroads => {}
-  }
+    }
 end
 
 def monopoly_with_second_tier
-  monopoly = {}
-  monopoly[:railroads] = {:pieces => 4}
+    monopoly = {
+    :railroads => {}
+    }
 end
 
 def monopoly_with_third_tier
